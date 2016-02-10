220e17ecbd9f9b2b665ddda7eeda53e167b1435a
Assem Zaitkaliyeva <Assem Zaitkaliyeva>
2016-02-11 05:43:27 +0600

    1

---
+++ Lab1.0/Lab1.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            for (int i = a; i <= b; i++) {
+                bool is_prime = true;
+                for (int j = 2; j*j<=i; j++) {
+                    if (i % j == 0)
+                        is_prime = false;
+                }
+                if (is_prime) {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
