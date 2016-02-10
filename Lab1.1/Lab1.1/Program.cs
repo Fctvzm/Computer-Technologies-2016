afef1185701428f3d82fe8ba49581a28eeb530e5
Assem Zaitkaliyeva <Assem Zaitkaliyeva>
2016-02-11 05:46:44 +0600

    added Complex

---
+++ Lab1.1/Lab1.1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._1
+{
+    class Complex
+    {
+        public int a, b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public override string ToString()
+        {
+            return a + "/" + b;
+        }
+
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex c3;
+            if (c1.b == c2.b)
+            {
+                c3 = new Complex(c1.a + c2.a, c1.b);
+            }
+            else
+            {
+                int i = c1.b * c2.b;
+                int j = c1.a * c2.b + c2.a * c1.b;
+                int x = i;
+                int y = j;
+
+                while (x != 0 && y != 0)
+                {
+                    if (x > y)
+                        x = x % y;
+                    else
+                        y = y % x;
+                }
+            
+                int k = x + y;
+                c3 = new Complex(j / k, i / k);
+            }
+        return c3;
+    }
+}
+class Program
+    {
+        static void Main(string[] args) {
+            int A, B, C, D;
+            A = int.Parse(Console.ReadLine());
+            B = int.Parse(Console.ReadLine());
+            C = int.Parse(Console.ReadLine());
+            D = int.Parse(Console.ReadLine());
+            Complex c = new Complex(A, B);
+            Complex d = new Complex(C, D);
+            Complex w = c + d;
+            Console.WriteLine(w);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
