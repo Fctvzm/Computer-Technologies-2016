e865735f91fad69cea98688f4df89b6732116f63
Assem Zaitkaliyeva <Assem Zaitkaliyeva>
2016-02-11 05:48:19 +0600

    added Student

---
+++ Lab1.2/Lab1.2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student a = new Student();
+            a.Name = "Assem";
+            a.Surname = "Zaitkaliyeva";
+            a.gpa = 4.0;
+            a.age = 17;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+
+    class Student
+    {
+        public string Name, Surname;
+        public int age;
+        public double gpa;
+
+        public override string ToString()
+        {
+            return this.Surname + " " + this.Name + " " + this.age+" "+this.gpa;
+        }
+    }
+}
